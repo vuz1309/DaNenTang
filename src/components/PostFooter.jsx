f9b6d78d39291a53d6e9377c16eea0ea560cb849
vuz1309 <vunguyenaz1309@gmail.com>
2023-10-29 09:29:58 +0700

    feat: like status

--- src/components/PostFooter.jsx
+++ src/components/PostFooter.jsx
@@ -1,5 +1,14 @@
-import {View, Text, Image, StyleSheet} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+  Modal,
+  Pressable,
+} from 'react-native';
+import React, {useState} from 'react';
 import Like from '../assets/images/like.jpeg';
 import Shock from '../assets/images/shock.jpeg';
 import Heart from '../assets/images/heart.jpeg';
@@ -7,6 +16,7 @@ import {Colors} from '../utils/Colors';
 import VectorIcon from '../utils/VectorIcon';
 
 const PostFooter = ({data}) => {
+  const [reactionModal, setReactionModal] = useState(false);
   return (
     <View style={styles.postFotterContainer}>
       <View style={styles.footerReactionSec}>
@@ -14,21 +24,46 @@ const PostFooter = ({data}) => {
           <Image source={Like} style={styles.reactionIcon} />
           <Image source={Shock} style={styles.reactionIcon} />
           <Image source={Heart} style={styles.reactionIcon} />
-          <Text style={{marginLeft: 4}}> 23</Text>
-          <Text style={styles.reactionCount}>{data.reactionCount}</Text>
+
+          <Text style={styles.reactionCount}> {data.reactionCount}</Text>
         </View>
         <Text style={styles.reactionCount}>{data.comments} Bình luận</Text>
       </View>
       <View style={styles.userActionSec}>
-        <View style={styles.row}>
-          <VectorIcon
-            name="like2"
-            type="AntDesign"
-            size={25}
-            color={Colors.grey}
-          />
-          <Text style={styles.reactionCount}>Thích</Text>
-        </View>
+        {reactionModal && (
+          <View style={styles.reactionModalContainer}>
+            <TouchableOpacity onPress={() => setReactionModal(false)}>
+              <View>
+                <Image source={Like} style={styles.reactionIcon} />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View>
+                <Image source={Shock} style={styles.reactionIcon} />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View>
+                <Image source={Heart} style={styles.reactionIcon} />
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
+
+        <TouchableOpacity
+          onPress={() => Alert.alert('like')}
+          onLongPress={() => setReactionModal(true)}>
+          <View style={styles.row}>
+            <VectorIcon
+              name="like2"
+              type="AntDesign"
+              size={25}
+              color={Colors.grey}
+            />
+            <Text style={styles.reactionCount}>Thích</Text>
+          </View>
+        </TouchableOpacity>
+
         <View style={styles.row}>
           <VectorIcon
             name="chatbox-outline"
@@ -83,6 +118,27 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     flexDirection: 'row',
     justifyContent: 'space-around',
+  },
+  reactionModalContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    gap: 8,
+    backgroundColor: Colors.white,
+    borderRadius: 12,
+    shadowColor: Colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    elevation: 5,
+    padding: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: -30,
+    left: 0,
+    zIndex: 5,
   },
 });
 
--- src/utils/Colors.js
+++ src/utils/Colors.js
@@ -2,6 +2,7 @@ export const Colors = {
   primaryColor: '#1877F2',
   white: '#FFFFFF',
   black: '#000000',
+  blue: 'blue',
   grey: '#3A3A3A',
   lightgrey: '#EBECF0',
   borderGrey: '#BEBEBE',
