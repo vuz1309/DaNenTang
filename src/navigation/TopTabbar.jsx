696d97661452eab37bb96061c03e4b7001ace63b
QuangTran <quangtrancnh@gmail.com>
2023-11-18 01:03:56 +0700

    fix chubedan

--- src/navigation/TopTabbar.jsx
+++ src/navigation/TopTabbar.jsx
@@ -1,49 +1,37 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import VectorIcon from '../utils/VectorIcon';
 import {Colors} from '../utils/Colors';
 import {TabData} from '../data/TabData';
-import Header from '../components/Header';
 
 const Tab = createMaterialTopTabNavigator();
 
 const TopTabBar = () => {
-  const [currentTab, setCurrentTab] = useState('Home');
-  return (
-    <>
-      {currentTab === 'Home' && <Header />}
-      <Tab.Navigator
-        screenOptions={() => ({
-          tabBarShowLabel: false,
-          tabBarActiveTintColor: Colors.primaryColor,
-          tabBarInactiveTintColor: Colors.grey,
-        })}>
-        {TabData.map(tab => (
-          <Tab.Screen
-            key={tab.id}
-            name={tab.name}
-            component={tab.route}
-            listeners={({route}) => ({
-              tabPress: () => {
-                // Cập nhật biến state khi người dùng chuyển tab
-                setCurrentTab(route.name);
-              },
-            })}
-            options={{
-              tabBarIcon: ({color, focused}) => (
-                <VectorIcon
-                  type={focused ? tab.activeiconType : tab.inactiveIconType}
-                  name={focused ? tab.activeIconName : tab.inactiveIconName}
-                  size={focused ? tab.size : tab.unFocusSize}
-                  color={color}
+    return (
+        <Tab.Navigator
+            screenOptions={() => ({
+                tabBarShowLabel: false,
+                tabBarActiveTintColor: Colors.primaryColor,
+                tabBarInactiveTintColor: Colors.grey,
+            })}>
+            {TabData.map(tab => (
+                <Tab.Screen
+                    key={tab.id}
+                    name={tab.name}
+                    component={tab.route}
+                    options={{
+                        tabBarIcon: ({color, focused}) => (
+                            <VectorIcon
+                                type={focused ? tab.activeiconType : tab.inactiveIconType}
+                                name={focused ? tab.activeIconName : tab.inactiveIconName}
+                                size={focused ? tab.size : tab.unFocusSize}
+                                color={color}
+                            />
+                        ),
+                    }}
                 />
-              ),
-            }}
-          />
-        ))}
-      </Tab.Navigator>
-    </>
-  );
-};
-
+            ))}
+        </Tab.Navigator>
+    );
+}
 export default TopTabBar;
