3304361002ab7c032410d9a513ce4f3124735760
vuz1309 <vunguyenaz1309@gmail.com>
2023-11-22 15:20:14 +0700

    feat: api config

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,1 +1,56 @@
 export const BE_URL = 'https://it4788.catan.io.vn';
+import axios from 'axios';
+
+import {getAccessToken} from '../storage/asyncStorage';
+
+/**
+ * Khởi tạo cách truyền và xử lí Rest-API
+ * @Author NTVu - MF1742 - 23/08/2023
+ * @param {import('axios').CreateAxiosDefaults} config
+ * @param {{auth: boolean, silent: boolean}} param2
+ * @returns {import('axios').AxiosInstance}
+ */
+export const createApiInstance = (
+  config: import('axios').CreateAxiosDefaults,
+  {auth = true, silent}: {auth?: boolean; silent?: boolean} = {},
+) => {
+  const api = axios.create(config);
+
+  api.interceptors.request.use(
+    async config => {
+      if (auth && config?.headers) {
+        config.headers['Authorization'] = 'Bearer ' + (await getAccessToken());
+      }
+      return config;
+    },
+    error => {
+      Promise.reject(error);
+    },
+  );
+  api.interceptors.response.use(
+    /**
+     * Nếu response nhận về là json về convert về dạng camelCase
+     * @author NTVu - 14/09/2023
+     * @param {import('axios').AxiosResponse} response
+     * @returns {import('axios').AxiosResponse}
+     */
+    response => {
+      return response;
+    },
+    /**
+     * Xử lí các trường hợp lỗi
+     * @author NTVu - 06/09/2023
+     * @param {import('axios').AxiosError} error
+     * @returns {{message: string, data: object}} data
+     */
+    error => {
+      if (!silent) {
+        console.log(error);
+      }
+
+      return Promise.reject(error);
+    },
+  );
+
+  return api;
+};
