b8c8fd49bc7ab4717ca05b8231a084eaccd8a07e
vuz1309 <vunguyenaz1309@gmail.com>
2023-11-26 01:59:59 +0700

    feat: api comment

---
+++ src/api/modules/comment.request.ts
@@ -0,0 +1,39 @@
+import {requestJSONWithAuth} from '../request';
+
+export const feelPost = (body: {id: string; type: string}) => {
+  return requestJSONWithAuth('/feel', body);
+};
+/**
+ * 
+ * @param body example "id": "1",
+  "content": "so good",
+  "index": "0",
+  "count": "10",
+  "mark_id": "1",
+  "type": "1"
+ * @returns 
+ */
+export const setMarkComments = (body: {
+  id: string;
+  content: string;
+  index: string;
+  count: string;
+  mark_id: string;
+  type: string;
+}) => {
+  return requestJSONWithAuth('/set_mark_comment', body);
+};
+/**
+ * 
+ * @param body EX: "id": "1",
+  "index": "0",
+  "count": "10"
+ * @returns 
+ */
+export const getMarkComments = (body: {
+  id: string;
+  index: string;
+  count: string;
+}) => {
+  return requestJSONWithAuth('/get_mark_comment', body);
+};
